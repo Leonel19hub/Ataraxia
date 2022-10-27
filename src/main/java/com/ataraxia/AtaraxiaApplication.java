58db18e08eab1f25db32083e872b16796d75f9a1
Leonel19Hub <leonel19coraite@gmail.com>
2022-10-27 19:06:41 -0300

    Inicializacion del proyecto

---
+++ src/main/java/com/ataraxia/AtaraxiaApplication.java
@@ -0,0 +1,13 @@
+package com.ataraxia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtaraxiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtaraxiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ataraxia/AtaraxiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ataraxia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtaraxiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
