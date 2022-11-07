efd94baa0b8df7955ca57c56925bb335562eb918
Nachox <Nachox@DESKTOP-NP450AC>
2022-11-07 16:51:46 -0300

    psicologo.model

--- src/main/java/com/ataraxia/model/Psicologo.java
+++ src/main/java/com/ataraxia/model/Psicologo.java
@@ -1,5 +1,28 @@
 package com.ataraxia.model;
 
-public class Psicologo {
+import javax.persistence.Entity;
+
+import org.springframework.stereotype.Component;
+
+@Entity
+@Component
+public class Psicologo extends Usuario {
+	
+	private String direccionTrabajo;
+	private String titulo;
+	
+	
+	public String getDireccionTrabajo() {
+		return direccionTrabajo;
+	}
+	public void setDireccionTrabajo(String direccionTrabajo) {
+		this.direccionTrabajo = direccionTrabajo;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
 
 }
