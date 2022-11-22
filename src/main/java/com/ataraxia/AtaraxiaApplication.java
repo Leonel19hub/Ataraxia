b171183d19685fcffccad5c8698ce7c0639b474f
Leonel19hub <leonel19coraite@gmail.com>
2022-11-22 20:33:25 -0300

    22/11 - 4

--- src/main/java/com/ataraxia/AtaraxiaApplication.java
+++ src/main/java/com/ataraxia/AtaraxiaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AtaraxiaApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AtaraxiaApplication.class, args);
 	}
--- src/main/java/com/ataraxia/model/Psicologo.java
+++ src/main/java/com/ataraxia/model/Psicologo.java
@@ -11,24 +11,62 @@ import org.springframework.stereotype.Component;
 @PrimaryKeyJoinColumn(referencedColumnName = "idUser")
 public class Psicologo extends Usuario { 
 	
-	private String direccionTrabajo;
+	// private String direccionTrabajo;
+	private String provincia;
+	private String localidad;
+	private String calle;
+	private int numero;
 	private String titulo;
 	
 	public Psicologo (){
 		super();
 	}
 
-	public String getDireccionTrabajo() {
-		return direccionTrabajo;
-	}
-	public void setDireccionTrabajo(String direccionTrabajo) {
-		this.direccionTrabajo = direccionTrabajo;
-	}
+	// public String getDireccionTrabajo() {
+	// 	return direccionTrabajo;
+	// }
+	// public void setDireccionTrabajo(String direccionTrabajo) {
+	// 	this.direccionTrabajo = direccionTrabajo;
+	// }
 	public String getTitulo() {
 		return titulo;
 	}
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
+	public String getProvincia() {
+		return provincia;
+	}
+
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+
+	public String getLocalidad() {
+		return localidad;
+	}
+
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+
+	public String getCalle() {
+		return calle;
+	}
+
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	
 
 }
